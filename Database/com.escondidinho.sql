05fc5abf3eefa5d220470a20d13f83d39f0356b2
Caio Amaral Corrêa <caiobep.dev@gmail.com>
2015-10-28 21:28:24 -0200

    Organização e Tela Admin
    Organização de todo o sistema
    Tela Main admin
    Tela Admin
    Fluxo Mental
    Banco de Dados
    Mapa Mental Admin

---
+++ Database/com.escondidinho.sql
@@ -0,0 +1,140 @@
+create database Escondidinho;
+
+use Escondidinho;
+
+create table empresa
+(
+id_emp int not null auto_increment,
+nome_emp varchar(30) not null,
+senha_emp varchar(12) not null,
+end_emp varchar(30) not null,
+num_emp int not null,
+compl_emp varchar(30) not null,
+bairro_emp varchar(30) not null,
+cidade_emp varchar(30) not null,
+uf_emp varchar(30) not null,
+cep_emp varchar(30) not null,
+email_emp varchar(30) not null,
+cnpj_emp varchar(20) not null,
+primary key (id_emp),
+constraint email_emp_uq unique(email_emp),
+constraint cnpj_emp_uq unique(cnpj_emp)
+);
+
+create table clientes
+(
+id_cli int not null auto_increment,
+nome_cli varchar(30) not null,
+senha_cli varchar(12) not null,
+end_cli varchar(30) not null,
+num_cli int not null,
+compl_cli varchar(30) not null,
+bairro_cli varchar(30) not null,
+cidade_cli varchar(30) not null,
+uf_cli varchar(30) not null,
+cep_cli varchar(30) not null,
+email_cli varchar(30) not null,
+cpf_cli varchar(20) not null,
+dtnasc_cli date not null,
+sex_cli varchar(30) not null,
+estciv_cli char(1) not null,
+primary key(id_cli),
+constraint email_cli_uq unique(email_cli),
+constraint cpf_cli_uq unique(cpf_cli),
+constraint sex_cli_chk check(sex_cli ('F','M','O')),
+constraint estciv_cli_chk check(estciv_cli ('C','S','O'))
+);
+
+create table Login
+(
+id_log int not null auto_increment,
+id_emp int not null,
+id_cli int not null,
+nome_log varchar(30) not null,
+senha_log varchar(15) not null,
+primary key(id_log),
+foreign key (id_emp) references empresa(id_emp),
+foreign key (id_cli) references clientes(id_cli)
+);
+
+
+create table pagamento
+(
+id_pag int not null auto_increment,
+forma_pag varchar(30),
+primary key (id_pag)
+);
+
+create table unidades
+(
+id_unidade int not null auto_increment,
+tipo_unidade varchar (30) not null,
+primary key (id_unidade)
+);
+
+create table tipos
+(
+id_tipo int not null auto_increment,
+tipo_produto varchar(30) not null,
+primary key (id_tipo)
+);
+
+create table padaria
+(
+id_pad int not null auto_increment,
+nome_pad varchar(30) not null,
+cep_pad varchar(30) not null,
+cnpj_pad varchar(30) not null,
+fone_pad varchar(15) not null,
+email_pad varchar(30) not null,
+primary key (id_pad),
+constraint cnpj_pad_uq unique(cnpj_pad)
+);
+
+create table produtos
+(
+id_prod int not null auto_increment,
+id_pad int not null,
+id_unidade int not null,
+id_tipo int not null,
+nome_prod varchar(30) not null,
+descr_prod varchar(50) not null,
+valor_prod int not null,
+primary key (id_prod),
+foreign key (id_pad) references padaria(id_pad),
+foreign key (id_unidade) references unidades(id_unidade),
+foreign key (id_tipo) references tipos(id_tipo),
+constraint ch_prod check(valor_prod >0)
+);
+
+create table vendas
+(
+id_venda int not null auto_increment,
+id_emp int not null,
+id_cli int not null,
+id_pag int not null,
+data_venda date not null,
+dataent_venda date not null,
+valor_venda int not null,
+primary key (id_venda),
+foreign key (id_emp) references empresa(id_emp),
+foreign key (id_cli) references clientes(id_cli),
+foreign key (id_pag) references pagamento(id_pag),
+constraint ch_venda check ( valor_venda>0 )
+);
+
+create table itens_vendas
+(
+id_venda int not null,
+id_prod int not null,
+qtd_itens int not null,
+primary key (id_venda,id_prod),
+constraint ch_itens check(qtd_itens)
+);
+
+create table prod_pad
+(
+id_pad int not null,
+id_prod int not null,
+primary key (id_pad,id_prod)
+);
---
+++ Website/webpages/Login/Login.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="webpages_Login_Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-br">
+<head runat="server">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,
+     initial-scale=1"/>
+    <link rel="stylesheet" type="text/css" href="stylesheet/style.css"/>
+    <link rel="icon" href="../../images/Main.png"/>
+    <title>Login</title>
+</head>
+<body>
+  <div id="menubar_main" class="menubar">
+   <nav>
+      <ul>
+        <li id="logo_img">
+           <a href="../../index.html">  
+                <img src="../../images/Main.png" alt="Home" width="60px" height="60px">
+            </a>
+            </li>
+            <li><a class="menubar_a" href="../Login/Login.aspx">Entrar</a></li>
+            <li><a class="menubar_a" href="../Cadastro/cadastro.aspx">Cadastre-se</a></li>
+            <li><a class="menubar_a" href="../sobre.html">Quem Somos</a></li>
+            <li><a class="menubar_a" href="../contato.html">Contato</a></li>
+            <li><a class="menubar_a" href="../join.html">Faça Parte</a></li>
+            <li><a id="loja_img" href="../loja.html">
+            <img src="../../images/Shop.png" alt="Loja" height="60" width="40"/> 
+               <a>
+            </li>
+      </ul>
+  </nav>
+    </div>
+    <header>
+       <div class="header_div">
+         <form class="frmLogin" runat="server">
+           <label>Entrar</label>
+         </br>
+           <asp:Textbox ID="inputlogin" placeholder="Login" runat="server"></asp:Textbox>
+         </br>
+           <asp:Textbox id="inputPassword" placeholder="Senha" runat="server" TextMode="password" ></asp:Textbox>
+         </br>
+           <asp:button id="btnlogin" runat="server" text="Entrar" />
+         </br>
+           <asp:Button id="btncadastro" runat="server" Text="Cadastre-se" PostBackUrl="~/Cadastro.aspx"/>
+         </form>
+        <img  id="header_img" alt="Project Escondidinho" src="../../images/login_bkg.png">
+       </div>
+    </header>
+    <main>
+      <div class="fotter">
+        <p class="fotter_mail">Email:<a class="fotter_mail" href="mailto: caiobep@me.com">Caiobep@me.com</a></p>
+
+        <p>Made by <a id="fotter_ioetep" target="_tab" href ="https://github.com/ioEtep">io.Etep</a></p>
+      </div>
+   </main>
+</body>
+</html>
